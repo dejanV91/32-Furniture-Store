7f267a78f229a722cc3e01235e0b96c7d52238ac
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-09 20:52:17 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const allProductsUrl = 'https://course-api.com/javascript-store-products'
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const singleProductUrl =
+  'https://course-api.com/javascript-store-single-product'
