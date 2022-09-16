e6a5fd4b777a8d105d00c37ea7239138153a3224
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-16 20:12:24 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const allProductsUrl = 'https://course-api.com/javascript-store-products'
 // 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
 const singleProductUrl =
   'https://course-api.com/javascript-store-single-product'
+
+  const toggleBtn = document.querySelector(".toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("show");
+  });
