202c4f1be1339b6da76da63a3c1c688a64200c83
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-17 20:57:48 +0200

    initial commit

--- app.js
+++ app.js
@@ -4,14 +4,25 @@ const allProductsUrl = 'https://course-api.com/javascript-store-products'
 const singleProductUrl =
   'https://course-api.com/javascript-store-single-product'
 
-  const toggleBtn = document.querySelector(".toggle");
+const toggleBtn = document.querySelector(".toggle");
+const cartIcon = document.querySelector(".cart-div");
 
-  toggleBtn.addEventListener("click", () => {
-    const modal = document.querySelector(".modal");
-    modal.classList.add("show");
+toggleBtn.addEventListener("click", () => {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("show");
 
-    const closeModal = document.querySelector(".modal-close");
-    closeModal.addEventListener("click", () => {
-      modal.classList.remove("show");
-    })
-  });
+  const closeModal = document.querySelector(".modal-close");
+  closeModal.addEventListener("click", () => {
+    modal.classList.remove("show");
+  })
+});
+
+cartIcon.addEventListener("click", () => {
+  const cartModal = document.querySelector(".cart-modal");
+  const cartOverlay = document.querySelector(".cart-overlay");
+  cartModal.classList.add("show");
+  cartOverlay.classList.add("show");
+});
+
+  
+
