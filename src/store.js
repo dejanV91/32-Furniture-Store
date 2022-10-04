a1825ecd7fdf361e7989fdbba7dab02207cb08c4
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-04 19:48:12 +0200

    initial commit

--- src/store.js
+++ src/store.js
@@ -12,9 +12,4 @@ const setupStore = (products) => {
   setStorageItem('store', store);
 };
 
-const findProduct = (id) => {
-  let product = store.find((product) => product.id === id);
-  return product;
-};
 
-export { store, setupStore, findProduct };
