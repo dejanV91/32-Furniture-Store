868ccf24acf59c9a3086fedb80fd581842ee69bf
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-07 15:34:09 +0200

    initial commit

--- src/cart/addCart.js
+++ src/cart/addCart.js
@@ -1,7 +1,11 @@
 import { findProduct } from "../setupStore.js";
-import { getStorageItems } from "../utils.js";
+import { getElement, getStorageItems } from "../utils.js";
 import { addToCartDOM } from "./addToCartDOM.js";
 import { openCart } from "./toggleCart.js";
+
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
 
 let cart = getStorageItems("cart");
 
@@ -13,8 +17,29 @@ const addToCart = (id) => {
         product = {...product, amount: 1}
         cart = [...cart, product]
         addToCartDOM(product);
+    }else{
+        const amount = increaseAmount(id);  
+        const item = [...cartItemsDOM.querySelectorAll(".cart-item-amount")]
+        const newAmount = item.find((elem) => elem.dataset.id === id); 
+        newAmount.textContent = amount;
     }
+
+
+
+    openCart();
     
+}
+
+const increaseAmount = (id) => {
+    let newAmount;
+    cart = cart.map((item) => {
+        if (item.id === id) {
+            newAmount = item.amount + 1;
+            item = {...item, amount: newAmount}
+        }
+        return item
+    });
+    return newAmount
 }
 
 export {addToCart,
