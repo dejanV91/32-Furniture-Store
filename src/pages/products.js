1ed3b70c0b924695c7723829d48ec2c70307c2f7
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-10 18:19:03 +0200

    initial commit

---
+++ src/pages/products.js
@@ -0,0 +1,11 @@
+import "../cart/addCart.js";
+import "../cart/toggleCart.js";
+import "../toggleSidebar.js";
+
+// Constants
+const loading = document.querySelector(".page-loading");
+
+// Code
+loading.style.display = "none";
+
+//Functions
