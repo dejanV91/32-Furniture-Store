c8d3bf94282d01a9ac86b27e824a2c4a9a03031d
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-10 19:40:11 +0200

    initial commit

---
+++ src/filters/search.js
@@ -0,0 +1,29 @@
+import { display } from "../display.js";
+import { store } from "../setupStore.js";
+import { getElement } from "../utils.js";
+
+const inputForm = getElement(".input-form");
+const searchInput = getElement(".search-input");
+const productsContainer = getElement(".products-container");
+
+inputForm.addEventListener("keyup", () => {
+    let products = store.filter((product) => {
+        const {name} = product;
+        let searchName = name.toUpperCase();
+        let check = searchInput.value.toUpperCase();
+        
+        let include = searchName.startsWith(check);
+    
+        if (include) {
+            return product
+        }
+    });
+
+    display(productsContainer, products);
+
+    if (products.length < 1) {
+        productsContainer.innerHTML = `<h3 class="filter-error">
+            sorry, no products matched your search</h3>;`
+        }
+    
+});
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,11 +1,21 @@
 import "../cart/addCart.js";
 import "../cart/toggleCart.js";
 import "../toggleSidebar.js";
+import "../filters/search.js"
+import { store } from "../setupStore.js";
+import { display } from "../display.js"
+import { getElement } from "../utils.js";
 
 // Constants
-const loading = document.querySelector(".page-loading");
+const loading = getElement(".page-loading");
+const productsContainer = getElement(".products-container");
 
 // Code
+window.addEventListener("DOMContentLoaded", () => {
+   display(productsContainer, store);
+    
+});
+
 loading.style.display = "none";
 
 //Functions
